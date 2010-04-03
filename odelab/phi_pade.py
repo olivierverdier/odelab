fixed scaling bug in phi_pade

odelab/phi_pade.py
@@ -126,9 +126,10 @@ where :data:`M` is a square array.
 		N = [Polynomial(np.convolve(Dr, C[m:m+d+1])[:d+1]) for m,Dr in enumerate(D)]
 		return N, [Polynomial(Dl) for Dl in D]
 	
-	def scaling(self, z):
+	@classmethod
+	def scaling(self, z, threshold=0):
 		norm = ninf(z)
-		return int(math.ceil(math.log(norm,2)))
+		return max(threshold, int(math.ceil(math.log(norm,2))))
 	
 	def eval_pade(self, z, s=None):
 		"""
@@ -270,21 +271,25 @@ def test_phi_1_mat():
 	computed = phi_l(z,1)
 	nt.assert_almost_equal(computed, expected)
 
-def test_phi_pade(k=8,d=10):
+def test_phi_pade(k=4,d=6):
 	"""
 	Test of the Padé approximation of :math:`φ_l` on matrices.
 	"""
-	z = .1*np.array([[1.,2.],[3.,1.]])
 	phi = Phi(k,d)
 	N,D = phi.pade
-	for l in range(k):
-		expected = phi_l(z,l)
-		Nz = simple_mul(N[l].coeffs, z)
-		Dz = simple_mul(D[l].coeffs, z)
-		computed = lin.solve(Dz,Nz)
-		nt.assert_almost_equal(computed, expected)
+	for z in  [.1*np.array([[1.,2.],[3.,1.]]), np.array([[.01]]), np.array([[.1]])]:
+		print z
+		phis = phi(z)
+		for l in range(1,k+1):
+			print l
+			expected = phi_l(z,l)
+			Nz = simple_mul(N[l].coeffs, z)
+			Dz = simple_mul(D[l].coeffs, z)
+			computed = lin.solve(Dz,Nz)
+			nt.assert_almost_equal(computed, expected)
+			nt.assert_almost_equal(expected, phis[l])
 
-def test_phi_eval_pade(k=8,d=6):
+def test_phi_eval_pade_mat(k=8,d=6):
 	z = .1*np.array([[1.,2.],[3.,1.]])
 	phi = Phi(k,d)
 	phi.eval_pade(z)
@@ -298,6 +303,10 @@ def test_phi_scaled(l=5,d=10):
 	computed = phi(z)[-1]
 	nt.assert_approx_equal(computed, expected)
 
+def test_scaling():
+	nt.assert_equal(Phi.scaling(1.), 0)
+	nt.assert_equal(Phi.scaling(3.), 2)
+	nt.assert_equal(Phi.scaling(.1), 0)
 
 def test_phi_scaled_mat(l=2,d=6):
 	z = np.array([[1.,2.],[3.,1.]])