9b7816ff2be8fcda5fd2ee471fc24ccff22938e8
Olivier Verdier <olivier.verdier@gmail.com>
2010-05-18 21:43:27 +0200

    first attempt at order class

---
+++ odelab/order.py
@@ -0,0 +1,94 @@
+# -*- coding: UTF-8 -*-
+from __future__ import division
+
+import numpy as np
+import numpy.linalg
+import pylab as pl
+
+import odelab.solver as slv
+from odelab.scheme import ode15s
+
+def linear_regression(x, y, do_plot=False):
+	from scipy.stats import linregress
+	reg = linregress(x, y)
+	slope = reg[0]
+	if do_plot:
+		pl.plot(x,reg[1] + reg[0] * x, linewidth=2., label='slope: %.1f' % slope)
+	return reg
+
+class OrderComputer(object):
+	"""
+	Compute the order of a given system.
+	"""
+	
+	def __init__(self, solver, system, info, solution):
+		self.solver = solver
+		self.system = system
+		self.info = info
+		self.solution = solution
+	
+	extra_div = 10 # quotient between minimal step size and reference step size (not used with ode15s)
+	
+	
+	
+	def initialize_solver(self, h):
+		self.solver.system = self.system
+		self.solver.initialize(**self.info)
+		self.solver.initialize(h=h)
+	
+	def error(self, N):
+		h = self.info['time']/N
+		self.initialize_solver(h)
+		try:
+			self.solver.run()
+		except (self.solver.Unstable, self.solver.FinalTimeNotReached):
+			return None
+		return np.log10(np.max(np.abs(self.solver.final()-self.solution)))
+	
+	def compute_errors(self, ks):
+		Ns = np.round(10**(ks))
+		errors = [(N, self.error(N)) for N in Ns]
+		self.kerrors = np.array([(np.log10(N),e) for (N,e) in errors if e is not None]).T
+	
+	def plot_error(self):
+		linear_regression(self.kerrors[0], self.kerrors[1], do_plot=True)
+		pl.plot(self.kerrors[0], self.kerrors[1], label=str(self.solver), marker='o')
+	
+	def order(self):
+		self.reg = linear_regression(self.kerrors[0], self.kerrors[1])
+		slope = self.reg[0]
+		return slope
+		
+
+class OrderFarm(object):
+	def __init__(self, solvers, system, info):
+		self.solvers = solvers
+		self.system = system
+		self.info = info
+	
+	def compute_exact(self, ref_solver=None):
+		"""
+		Compute the exact solution with ode15s.
+		"""
+		self.ref_solver = slv.SingleStepSolver(ode15s(atol=1.e-15, rtol=5.e-14), self.system)
+		self.ref_solver.initialize(**self.info)
+		self.ref_solver.initialize(h = self.info['time']/10)
+## 		self.ref_solver.initialize(h=self.info['h']/(self.div_coeff**self.max_power)/self.extra_div)
+		self.ref_solver.run()
+		self.solution = self.ref_solver.final()
+
+	def initialize(self):
+		self.compute_exact()
+		self.order_computers = [OrderComputer(solver, self.system, self.info, self.solution) for solver in self.solvers]
+		
+	def run(self, ks):
+		for c in self.order_computers:
+			c.compute_errors(ks)
+	
+	def orders(self):
+		return [(c.solver, c.order()) for c in self.order_computers]
+	
+	def plot(self):
+		for c in self.order_computers:
+			c.plot_error()
+		
---
+++ tests/test_order.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+from __future__ import division
+
+if __name__ == '__main__':	
+	import sys
+	for mod in ['odelab.order', 'odelab.system', 'odelab.solver', 'odelab.phi_pade', 'odelab.scheme.exponential']:
+		try:
+			del sys.modules[mod]
+		except KeyError:
+			pass
+
+
+from odelab.order import *
+from odelab.solver import *
+from odelab.scheme.exponential import *
+from odelab.system import *
+
+class Test_Burgers(object):
+	def test_run(self):
+		B = BurgersComplex(viscosity=.03)
+		size = B.size
+		u0 = .5 - np.abs(B.points)
+## 		u0 = np.cos(B.points*(2*np.pi))*(1+np.sin(B.points*(2*np.pi)))
+		info = {'u0':u0, 't0':0., 'time':.5, }
+		
+		solvers = [MultiStepSolver(scheme_class(), B) for scheme_class in [LawsonEuler, RKMK4T, HochOst4, GenLawson45,ABNorset4,Lawson4,ABLawson4][3:4]]
+		self.o = OrderFarm(solvers, B, info)
+		self.o.initialize()
+		self.o.run(np.linspace(2,3,5))
+		print self.o.orders()
+		self.o.plot()
+
+if __name__ == '__main__':
+	t = Test_Burgers()
+	t.test_run()
