99f974033ddf60870bd13f726b2976925cd13106
Olivier Verdier <olivier.verdier@gmail.com>
2010-12-04 17:43:02 +0100

    remove trailing tabs

--- tests/test_dae.py
+++ tests/test_dae.py
@@ -19,7 +19,7 @@ class Vlin(object):
 	def __init__(self, t0, V0):
 		self.t0 = t0
 		self.V0 = V0
-	
+
 	def __call__(self,t):
 		t0 = self.t0
 		V0 = self.V0
@@ -34,7 +34,7 @@ class Vlin(object):
 		if 4*t0 <= t:
 			return 0
 
-		
+
 
 class SimpleDiff(System):
 	def __init__(self, V, gain=1e8):
@@ -42,18 +42,18 @@ class SimpleDiff(System):
 		self.gain = gain
 		self.mass_mat = np.array([[1.,-self.gain],[0,0]])
 		self.det_mat = np.array([[-self.gain, 0],[0,1.]])
-	
-	
+
+
 	def deterministic(self,t,u):
 		return np.dot(self.det_mat,u) - np.array([0,self.V(t)])
-	
+
 	def mass(self,t,u):
 		return np.dot(self.mass_mat,u)
-	
+
 	def noise(self, t,u):
 		return np.zeros_like(u)
-	
-	
+
+
 class Test(object):
 	@nt.raises(RootSolverDidNotConverge)
 	def test_run(self):
@@ -61,11 +61,4 @@ class Test(object):
 		self.s = SingleStepSolver(EulerMaruyama(), sys)
 		self.s.initialize(u0=np.array([0.,0]),h=2.5e-11,time=2.5e-8)
 		self.s.run()
-		
 
-if __name__ == '__main__':
-	t = Test()
-	t.test_run()
-	import pylab as pl
-	pl.clf()
-	t.s.plot()
