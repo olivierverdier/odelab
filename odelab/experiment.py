015f776d848fc83e932286fb4b6af2e67feded7f
Olivier Verdier <olivier.verdier@gmail.com>
2011-04-22 20:11:18 +0200

    new Experiment class

---
+++ odelab/experiment.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# −*− coding: UTF−8 −*−
+from __future__ import division
+
+import os
+
+import shelve
+import time
+import datetime
+from datetime import timedelta
+
+from pprint import PrettyPrinter
+
+pprinter = PrettyPrinter()
+
+class Experiment(object):
+	def __init__(self, parameters, store_prefix=''):
+		self.parameters = parameters
+		self.store_prefix = store_prefix
+
+	def get_system(self):
+		sys_class = self.parameters['system']
+		sys = sys_class(**self.parameters.get('system_params', {}))
+		return sys
+
+	def get_solver(self):
+		parameters = self.parameters
+		sys = self.get_system()
+		scheme_class = parameters['scheme']
+		scheme = scheme_class(**parameters.get('scheme_params', {}))
+		solver_class = parameters['solver']
+		solver = solver_class(scheme, sys)
+		return solver
+
+	def run(self, save=True):
+		parameters = self.parameters
+		self.solver = self.get_solver()
+		self.solver.initialize(**parameters['initialize'])
+		print '-'*80
+		print self.get_path()
+		print parameters['name']
+		pprinter.pprint(parameters)
+		start_time = time.time()
+		try:
+			self.solver.run()
+		except Exception as e:
+			print e
+		finally:
+			end_time = time.time()
+			self.duration = end_time - start_time
+			print str(timedelta(seconds=self.duration))
+			self.timestamp = datetime.datetime.now()
+			if save:
+				self.save()
+
+	def get_path(self):
+		path = os.path.join(self.store_prefix, self.parameters['family'])
+		return path
+
+
+	def save(self):
+		parameters = self.parameters
+		path = self.get_path()
+		shelf = shelve.open(path)
+		data = {}
+		data['timestamp'] = self.timestamp
+		data['duration'] = self.duration
+		data['params'] = parameters
+		data['events'] = self.solver.events_array
+		shelf[parameters['name']] = data
+		shelf.close()
+
+	@classmethod
+	def load(self, family, name):
+		shelf = shelve.open(family)
+		data = shelf[name]
+		params = data['params']
+		experiment = self(params)
+		solver = experiment.get_solver()
+		solver.load_data(data['events'])
+		shelf.close()
+		return solver
+
+
