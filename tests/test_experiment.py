e645b23391988a0cda358b069601e06ac7ba3abb
Olivier Verdier <olivier.verdier@gmail.com>
2011-05-31 11:14:10 +0200

    test_experiment is now a class

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -4,7 +4,6 @@ from __future__ import division
 
 from odelab.solver import SingleStepSolver
 from odelab.system import System
-from odelab.scheme import ExplicitEuler
 from odelab.experiment import Experiment
 
 import numpy as np
@@ -13,25 +12,29 @@ import nose.tools as nt
 def f(t,u):
 	return -u
 
-def test_experiment():
-	params = {
-		'family': 'tmpbank',
-		'name': 'tmpexp',
-		'system': System,
-		'system_params': {'f': f},
-		'solver': SingleStepSolver,
-		'scheme': ExplicitEuler,
-		'scheme_params': {},
-		'initialize': {
-			'u0' : np.array([1.]),
-			'time': 1.,
-			'h': .1,
-			},
-		}
-	exp = Experiment(params)
-	exp.run()
-	s = Experiment.load('tmpbank', 'tmpexp')
-	nt.assert_true(isinstance(s, SingleStepSolver))
-	nt.assert_true(isinstance(s.scheme, ExplicitEuler))
-	nt.assert_equal(len(s), 11)
+class Test_Experiment(object):
+	def setUp(self):
+		from odelab.scheme import ExplicitEuler
+		params = {
+			'family': 'tmpbank',
+			'name': 'tmpexp',
+			'system': System,
+			'system_params': {'f': f},
+			'solver': SingleStepSolver,
+			'scheme': ExplicitEuler,
+			'scheme_params': {},
+			'initialize': {
+				'u0' : np.array([1.]),
+				'time': 1.,
+				'h': .1,
+				},
+			}
+		exp = Experiment(params)
+		exp.run()
+
+	def test_load(self):
+		s = Experiment.load('tmpbank', 'tmpexp')
+		nt.assert_true(isinstance(s, SingleStepSolver))
+		nt.assert_equal(s.scheme.__class__.__name__, 'ExplicitEuler')
+		nt.assert_equal(len(s), 11)
 
