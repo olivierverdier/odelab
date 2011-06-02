fix chaotic particle system:  * energy is now right  * average force is computed

odelab/system/nonholonomic.py
@@ -280,8 +280,24 @@ class SinePendulum(Pendulum):
 		x,y = self.position(u)
 		return y - np.sin(x)
 
+def average_x_sqsq_force(x0,x1,y0,y1):
+	"""
+	mean force on the x component for the potential -x^2y^2
+	"""
+	return (x0*y0**2 + x1*y1**2)/4 + (2*y0*y1*(x0+x1) + x0*y1**2 + x1*y0**2)/12
+
+def add_average_sqsq_component(f,q0,q1,i,j):
+	"""
+	mean force for a potential - q_i^2 q_j^2
+	"""
+	f[i] += average_x_sqsq_force(q0[i],q1[i],q0[j],q1[j])
+	f[j] += average_x_sqsq_force(q0[j],q1[j],q0[i],q1[i])
+
 class ChaoticOscillator(NonHolonomic):
 	def __init__(self, size=3):
+		"""
+:param size: the unerlying size; the total dimension is 2n+1 + 2n+1 + 1 = 4n+3; that is 15 for the default size n=3
+		"""
 		self.size = size
 
 	def position(self,u):
@@ -308,6 +324,17 @@ class ChaoticOscillator(NonHolonomic):
 		force[n+1:2*n+1] += y*y*z
 		return -force
 
+	def average_force(self, u0, u1):
+		q0 = self.position(u0)
+		q1 = self.position(u1)
+		n = self.size
+		force = (q0 + q1)/2.
+		add_average_sqsq_component(force,q0,q1,n+1,n+2)
+		for i in range(n):
+			add_average_sqsq_component(force,q0,q1,1+i,1+n+i)
+		return -force
+
+
 	def codistribution(self, u):
 		q = self.position(u)
 		n = self.size
@@ -320,7 +347,7 @@ class ChaoticOscillator(NonHolonomic):
 		n = self.size
 		y = q[1:n+1]
 		z = q[n+1:2*n+1]
-		return .5*(np.sum(v*v,axis=0) + np.sum(y*y*z*z, axis=0) + z[0]*z[1])
+		return .5*(np.sum(v*v,axis=0) + np.sum(q*q,axis=0) + np.sum(y*y*z*z, axis=0) + z[0]**2*z[1]**2)
 
 class Chaplygin(NonHolonomic):
 	"""

tests/scheme/test_constrained.py
@@ -189,13 +189,38 @@ def test_pendulum_NHE():
 	s.initialize(np.array([1.,0,0,0,0]))
 	s.run()
 
-def test_chaoticosc_ML():
-	s = SingleStepSolver(McLachlan(), ChaoticOscillator(3))
-	u0 = np.zeros(15)
-	u0[0] = 1.
-	s.initialize(u0=u0, h=.1, time=10)
-	s.run()
-	print s.system.energy(s.final())
+class Harness_chaoticosc(object):
+	def setUp(self):
+		s = SingleStepSolver(self.scheme, ChaoticOscillator(3))
+		u0 = np.zeros(15)
+		N = 10 - 1
+		n = 2 # j in range(N+1)
+		angle = n*np.pi/2/N
+		u0[:7] = np.array([np.cos(angle),.6,.4,.2,1.,1.,1.])
+		u0[7:9] = np.array([0., np.sin(angle)])
+		s.initialize(u0=u0, h=.05, time=1)
+		self.s = s
+
+	def test_run(self):
+		s = self.s
+		s.run()
+		print s.system.energy(s.final())
+
+	def test_energy(self):
+		self.s.run()
+		H1 = self.s.system.energy(self.s.final())
+		H0 = self.s.system.energy(self.s.initial())
+		print H0
+		print H1
+		npt.assert_almost_equal(H1, H0, decimal=self.energy_tol)
+
+class Test_chaotic_ML(Harness_chaoticosc):
+	scheme = McLachlan()
+	energy_tol = 2
+
+class Test_chaotic_H(Harness_chaoticosc):
+	scheme = NonHolonomicEnergy()
+	energy_tol = 10
 
 class Harness_Chaplygin(object):
 	def setUp(self):