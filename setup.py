bab3f4740cdd0105facabd6eb85cc42e78dcae48
Olivier Verdier <olivier.verdier@gmail.com>
2012-04-21 17:43:34 +0200

    add setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(
+	name = 'odelab',
+	description = 'ODE Simulation Tools',
+	author='Olivier Verdier',
+	license = 'BSD2',
+	keywords = ['Math', 'ode',],
+	
+	packages=['odelab', 'odelab.scheme', 'odelab.system', 'odelab.system.nonholonomic'],
+	classifiers = [
+	'Development Status :: 4 - Beta',
+	'Intended Audience :: Science/Research',
+	'License :: OSI Approved :: BSD License',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python',
+	'Topic :: Scientific/Engineering :: Mathematics',
+	],
+	)
