2fcc8238464a2e167ffefae3b6b973b2e3eeed3c
Olivier Verdier <olivier.verdier@gmail.com>
2016-06-21 12:49:08 +0200

    move some obsolete tests to test_order

--- tests/test_order.py
+++ tests/test_order.py
@@ -21,3 +21,51 @@ class Order_Burgers(object):
 		print(self.o.orders())
 		self.o.plot()
 
+
+def make_lin(A):
+	if np.isscalar(A):
+		def lin(t,u):
+			return A*u
+	else:
+		def lin(t, u):
+			return dot(A,u)
+	lin.exact = make_exp(A)
+	return lin
+
+def make_exp(A):
+	if np.isscalar(A):
+		def exact(u0,t0,t):
+			return u0 * exp((t-t0)*A)
+	else:
+		def exact(u0, t0, t):
+			return dot(expm((t-t0)*A),u0)
+	return exact
+
+class Harness_Solver_Order(object):
+	no_plot = True
+	a = -1.
+	u0 = 1.
+	time = 1.
+	do_plot=False
+	def notest_order(self):
+		self.solver.initialize(u0=self.u0, time=self.time)
+		order = self.solver.plot_error(do_plot=self.do_plot)
+		print(order)
+		nt.assert_true(order < self.order + .1)
+
+class Test_ExplicitEuler(Harness_Solver_Order):
+	def setUp(self):
+		self.solver = Solver(ExplicitEuler(h=.1), System(make_lin(self.a)))
+		self.order = -1.
+
+class Test_ImplicitEuler(Harness_Solver_Order):
+	def setUp(self):
+		self.solver = Solver(ImplicitEuler(h=.1), System(make_lin(self.a)))
+		self.order = -1.
+
+class Test_RungeKutta4(Harness_Solver_Order):
+	def setUp(self):
+		self.solver = Solver(RungeKutta4(h=.1), System(make_lin(self.a)))
+		self.solver.err_kmin = 1
+		self.solver.err_kmax = 2.5
+		self.order = -4.
