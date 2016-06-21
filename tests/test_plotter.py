b16a6a3dce701600b83c16b2ac7acd608aecf1b1
Olivier Verdier <olivier.verdier@gmail.com>
2016-06-21 08:39:43 +0200

    test_plotter: pytest fixtures

--- tests/test_plotter.py
+++ tests/test_plotter.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 
-
+import pytest
 
 from odelab.scheme import *
 from odelab.scheme.classic import *
@@ -52,82 +52,74 @@ def quick_setup(plotter, **kwargs):
 	for k,v in kwargs.items():
 		setattr(plotter,k,v)
 
-class Harness_Circle(object):
-	def setUp(self):
-		self.f = rotational
-		self.set_scheme()
-		self.s = SingleStepSolver(self.scheme, DummySystem(self.f))
-		self.scheme.h = .05
-		self.s.initialize(u0 = array([1.,0.]))
-		self.s.run(time = 3.)
+schemes = [ExplicitEuler(), ImplicitEuler(), RungeKutta4()]
 
-	def test_plot_2D(self):
+@pytest.fixture(scope='module', params=schemes, ids=repr)
+def solver(request):
+	f = rotational
+	scheme = request.param
+	solver = SingleStepSolver(scheme, DummySystem(f))
+	scheme.h = .05
+	solver.initialize(u0 = array([1.,0.]))
+	solver.run(time = 3.)
+	return solver
+
+class TestCircle(object):
+
+	def test_plot_2D(self, solver):
 		plt.clf()
-		a = self.s.plot(1,time_component=0).axis
-		self.assertEqual(a.get_xlabel(), 'x')
-		self.s.plot2D()
+		a = solver.plot(1,time_component=0).axis
+		assert a.get_xlabel() == 'x'
+		solver.plot2D()
 		for l in a.get_lines():
 			d = l.get_data()
 			radii = np.abs(np.sqrt(d[0]**2+d[1]**2) - 1)
 			assert np.all(radii < .3) # should roughly be a circle
 
-	def test_plot(self):
-		a = self.s.plot(plot_exact=False).axis
-		self.assertEqual(a.get_xlabel(), 'time')
-		self.s.plot(plot_exact=True)
-		plotter = Plotter(self.s)
+	def test_plot(self, solver):
+		a = solver.plot(plot_exact=False).axis
+		assert a.get_xlabel() == 'time'
+		solver.plot(plot_exact=True)
+		plotter = Plotter(solver)
 		plotter.axis_plot()
 		quick_setup(plotter, components=['output', 0], plot_exact=False)
 		plt.gca().cla()
 		plotter.axis_plot()
-		self.assertEqual(len(plotter.axis.lines), 2)
+		assert len(plotter.axis.lines) == 2
 		quick_setup(plotter, components='output',)
 		plt.gca().cla()
 		plotter.axis_plot()
 		quick_setup(plotter, components=['output', 0], plot_exact=True)
 		plt.gca().cla()
 		plotter.axis_plot()
-		self.assertEqual(len(plotter.axis.lines), 4)
+		assert len(plotter.axis.lines) == 4
 		# the following should be tested:
-		self.s.plot(components=['output'], error=True)
-		self.s.plot_function('output')
+		solver.plot(components=['output'], error=True)
+		solver.plot_function('output')
 		quick_setup(plotter, components=[observer], plot_exact=True)
-		# plotter.savefig(path)
 
-	def test_resolution(self, max_res=10):
-		p = self.s.plot(plot_exact=False)
+	def test_resolution(self, solver, max_res=10):
+		p = solver.plot(plot_exact=False)
 		p.max_plot_res = max_res
 		p.plot()
 		a = p.axis
 		nb_points = a.get_lines()[-1].get_data()
-		self.assertLessEqual(len(nb_points), max_res)
+		assert len(nb_points) <= max_res
 
-	def test_t0(self):
+	def test_t0(self, solver):
 		t0 = 1
-		p = self.s.plot(t0=t0)
+		p = solver.plot(t0=t0)
 		p.plot()
 		a = p.axis
 		data = a.get_lines()[-1].get_data()
 		## bound = a.get_xbound()
 		bound = data[0][0]
-		self.assertAlmostEqual(bound,t0, places=0)
+		assert round(bound - t0, 0) == 0
 
-	def test_time(self):
+	def test_time(self, solver):
 		time = 2.
-		p = self.s.plot(time=time)
+		p = solver.plot(time=time)
 		a = p.axis
 		data = a.get_lines()[-1].get_data()
-		self.assertLessEqual(data[0][-1],time)
+		assert data[0][-1] <= time
 
-class Test_Circle_EEuler(Harness_Circle, unittest.TestCase):
-	def set_scheme(self):
-		self.scheme = ExplicitEuler()
-
-class Test_Circle_IEuler(Harness_Circle, unittest.TestCase):
-	def set_scheme(self):
-		self.scheme = ImplicitEuler()
-
-
-class Test_Circle_RK34(Harness_Circle, unittest.TestCase):
-	def set_scheme(self):
-		self.scheme = RungeKutta34()
